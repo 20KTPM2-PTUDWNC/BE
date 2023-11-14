63493531e6dd7aa0eded36a458d236c104bf4a42
ngochai-hcmus <hainguyen13097@gmail.com>
2023-11-14 20:28:20 +0700

    Fix bug authentication

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 import * as express from 'express';
 import authRoute from "./auth.js";
 import userRoute from "./user.js";
-import { authentication } from '../middlewares/authentication.js';
 
 const router = express.Router();
 
 router.use('/', authRoute);
-
-router.use(authentication);
 
 router.use('/', userRoute);
 
