de2b0816cdc9b14c73dbc5a74c9cf2ebf7978d89
LeThuyNga1606 <lethuynga1662002@gmail.com>
2023-11-25 08:03:33 +0700

    Show member in class successfully

--- controllers/userClass.js
+++ controllers/userClass.js
@@ -1,14 +1,28 @@
 import userClassModel from '../models/userClass.js';
+import userService from '../services/users.js';
+import userModel from '../models/users.js';
 
 const showMemberList = async(req, res, next) => {
     const classId = req.params.classId;
-    const user = await userClassModel.find({classId: classId});
+    const users = await userClassModel.find({classId: classId}).lean();
+    let listTeacher = [];
+    let listStudent = [];
+    let listUser;
 
-    if (user) {
-        return res.status(200).json(user);
-    } 
-    else{
-        res.status(400).json({message: `No class with id: ${classId} `});
-    }   
+    for(let i = 0; i < users.length; i++){
+        if (users[i].userRole === 1){
+            const teacher = await userService.findUserById({_id: users[i].userId});
+            listTeacher = [...listTeacher, teacher];
+        }
+        else{
+            const student = await userService.findUserById({_id: users[i].userId});
+            listStudent = [...listStudent, student];
+        }
+        listUser = {
+            teachers: listTeacher,
+            students: listStudent
+        };
+    }
+    return res.status(200).json(listUser);
 }
 export {showMemberList};
