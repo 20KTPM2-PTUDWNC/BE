d8319115b0f281df525b19175a342be47e280a42
ngochai-hcmus <hainguyen13097@gmail.com>
2023-12-02 19:28:28 +0700

    Add log passport jwt

--- config/passport.js
+++ config/passport.js
@@ -5,39 +5,39 @@ import passportFacebook from 'passport-facebook';
 import usersService from "../services/users.js";
 
 const cookieExtractor = function (req) {
-  let token = null;
-  if (req.headers['authorization'] && typeof req.headers['authorization'] === 'string') {
-    token = req.headers['authorization'].split(' ')[1];
-  }
-  return token;
+  console.log('authorization', req.headers['authorization']);
+  return req.headers['authorization'];
 };
 
 const JwtStrategy = passportJwt.Strategy;
 passport.use(new JwtStrategy({
-    secretOrKey: process.env.SECRET_KEY,
-    jwtFromRequest: cookieExtractor
+  secretOrKey: process.env.SECRET_KEY,
+  jwtFromRequest: cookieExtractor
 }, async (payload, done) => {
-    try {
+  try {
 
-        if (!payload) {
-            return done(null, false);
-        }
-
-        return done(null, payload);
-    } catch (error) {
-        return done(error, false);
+    if (!payload) {
+      return done(null, false);
     }
+
+    console.log('payload', payload);
+
+    return done(null, payload);
+  } catch (error) {
+    console.log('error', error);
+    return done(error, false);
+  }
 }));
 
 const GoogleStrategy = passportGoogle.Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/v1/auth/google/callback",
-    passReqToCallback   : true
-  },
-  async function(request, accessToken, refreshToken, profile, done) {
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "/v1/auth/google/callback",
+  passReqToCallback: true
+},
+  async function (request, accessToken, refreshToken, profile, done) {
     // findOrCreate
     const user = await usersService.findOrCreate(profile);
     return done(null, user);
@@ -46,12 +46,12 @@ passport.use(new GoogleStrategy({
 
 const FacebookStrategy = passportFacebook.Strategy;
 passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "/v1/auth/facebook/callback",
-    profileFields: ['id', 'email', 'displayName', 'photos']
-  },
-  async function(accessToken, refreshToken, profile, cb) {
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: "/v1/auth/facebook/callback",
+  profileFields: ['id', 'email', 'displayName', 'photos']
+},
+  async function (accessToken, refreshToken, profile, cb) {
     const user = await usersService.findOrCreate(profile);
     return cb(null, user);
   }
