4ddc5c9fdf5857b924cdd2faed5d8b219f6d8d85
ngochai-hcmus <hainguyen13097@gmail.com>
2023-12-18 22:51:46 +0700

    Fix bug mapping student id not create student class

---
+++ models/studentClass.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const studentClassSchema = new mongoose.Schema({
+    classId:{
+        type: mongoose.Types.ObjectId,
+        ref: "Class",
+        required: true,
+    },
+    studentId: {
+        type: Number,
+        default: null,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    deleteAt: {
+        type: Date,
+        default: null
+    }
+},
+    { timestamps: true });
+
+export default mongoose.model('StudentClass', studentClassSchema, 'studentclass');
